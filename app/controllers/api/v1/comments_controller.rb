a9d4fc802c2d01db3fb9ae626f5bda5e19c51339
usernameBakhtiyor <harhil46@gmail.com>
2022-12-10 18:57:50 +0500

    Fix linter errors

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,9 +6,11 @@ class Api::V1::CommentsController < Api::V1::ApplicationController
     comments = @post.comments
     render json: comments, status: :ok
   end
+
   def show
     render json: @comment, status: :ok
   end
+
   def create
     # should use current_user
     @comment = @author.comments.new(post: @post, **comment_params)
@@ -18,19 +20,23 @@ class Api::V1::CommentsController < Api::V1::ApplicationController
       render json: @comment.errors, status: :unprocessable_entity
     end
   end
+
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_author
     @author = User.find(params[:user_id])
   end
+
   def set_post
     @post = set_author.posts.find(params[:post_id])
   end
+
   def set_comment
     @comment = set_post.comments.find(params[:id])
   end
+
   def comment_params
     params.require(:comment).permit(:text)
   end
-
 end
