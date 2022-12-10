03a3bb938635da6ab744f5870e3d6041b5bbd1cd
usernameBakhtiyor <harhil46@gmail.com>
2022-12-10 21:04:12 +0500

    Fix errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,43 +1,34 @@
 class Api::V1::UsersController < Api::V1::ApplicationController
   before_action :set_author, only: [:show]
-
-  def index
-    authors = User.all.order(:id)
-    render json: authors, status: :ok
-  end
+  before_action :authorize_request, except: :login
+  include JsonWebToken
 
   def show
     render json: @author, status: :ok
   end
 
-  include JsonWebToken
-  class UsersController < ApiController
-    before_action :authorize_request, except: :login
-    def index
-      users = User.all
-      render json: { status: 'SUCCESS', message: 'Loaded users successfully', data: users }, status: :ok
-    end
+  def index
+    users = User.all
+    render json: { status: 'SUCCESS', message: 'Loaded users successfully', data: users }, status: :ok
+  end
 
-    def login
-      @user = User.find_by_email(params[:email])
-      if @user
-        token = jwt_encode(user_id: @user.id)
-        time = Time.now + 7.days.to_i
-        render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
-                       Name: @user.name }, status: :ok
-      else
-        render json: { error: 'unauthorized' }, status: :unauthorized
-      end
-    end
-
-    private
-
-    def login_params
-      params.permit(:email, :password)
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user
+      token = jwt_encode(user_id: @user.id)
+      time = Time.now + 7.days.to_i
+      render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
+                     Name: @user.name }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
     end
   end
 
   private
+
+  def login_params
+    params.permit(:email, :password)
+  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_author
